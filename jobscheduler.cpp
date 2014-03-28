ab264e59134408f924757720f39a4bf193e12217
stamaimer <stamaimer@gmail.com>
2014-03-28 10:50:26 +0800

    before getSKU

--- jobscheduler.cpp
+++ jobscheduler.cpp
@@ -52,7 +52,7 @@ bool JobScheduler::getJsonDoc(QNetworkReply* reply, Synnex synnex, QString FUNCT
         {
             qDebug() << __TIME__ << FUNCTION << parse_error.error << parse_error.errorString();
 
-            //
+            //发生解析错误之时采取什么措施？
 
             return false;
         }
@@ -106,23 +106,54 @@ void JobScheduler::getCategories(QNetworkReply* reply, Synnex synnex)
 {
     if(getJsonDoc(reply, synnex, __FUNCTION__))
     {
+        //----------------------------------------------------------------------------------------------//
+
+        QString request_url = "http://ec.synnex.com/ec-mobile-new/mobile/product.do?method=search";
+
+        QMap<QByteArray, QByteArray> request_headers;
+
+        request_headers["cookie"] = JobScheduler::cookie;
+
+        request_headers["content-type"] = "application/x-www-form-urlencoded";
+
+        QByteArray request_body = "mobile_device=" + mobile_device + "&mobile_os=" + mobile_os
+                                + "&begin=0&offset=30000&familyId=%1&catId=%2&spaType=VS";
+
+        QString pattern = request_body;
+
+        //----------------------------------------------------------------------------------------------//
+
         QJsonObject obj = doc.object();
 
         QJsonArray data = obj["data"].toArray();
+
+        qDebug() << __TIME__ << __FUNCTION__;
 
         for(int i = 0; i < data.size(); ++i)
         {
             QJsonObject family = data[i].toObject();
 
-            qDebug() << family["familyId"].toString()
+            qDebug() << "\t\t"
+                     << family["familyId"].toString()
                      << family["familyDesc"].toString();
 
             QJsonArray sub_cats = family["subCats"].toArray();
 
             for(int j = 0; j < sub_cats.size(); ++j)
             {
-                qDebug() << '\t' << sub_cats[j].toObject()["catId"].toString()
+                qDebug() << "\t\t\t"
+                         << sub_cats[j].toObject()["catId"].toString()
                          << sub_cats[j].toObject()["catDesc"].toString();
+
+                //-------------------------------------------------------------------/
+
+                request_body = pattern.arg(family["familyId"].toString())
+                                      .arg(sub_cats[j].toObject()["catId"].toString())
+                                      .toUtf8();
+
+                synnexs.append(Synnex(request_url, request_headers, request_body, 2));
+
+                //-------------------------------------------------------------------/
             }
         }
     }
