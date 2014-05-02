60f507c6a80907f067cf53d00b25fc4f29896767
stamaimer <stamaimer@gmail.com>
2014-05-02 21:54:57 +0800

    utils

--- utils.h
+++ utils.h
@@ -1,36 +1,69 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <QObject>
 #include <QVector>
 #include <QString>
 
+#include <QEventLoop>
+
 #include <QNetworkProxy>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QNetworkAccessManager>
 
-#include <stdlib.h>
-#include <time.h>
+//class Utils
+//{
+//    //Q_OBJECT
 
-class Utils
-{
-public:
+//    static QNetworkRequest       request;
+//    static QNetworkReply*        reply;
+//    static QNetworkAccessManager manager;
 
-    static void toggle(QVector<QString> ips, QNetworkProxy proxy)
-    {
-        srand(time(NULL));
+//    static QEventLoop synchronous;
 
-        int index = rand() % ips.size();
+//public:
 
-        QString ip   = ips[index].split(':')[0];
-        QString port = ips[index].split(':')[1];
+//    static void toggle(QNetworkProxy proxy)
+//    {
+//        request.setUrl(QUrl("http://www.iphai.com/getapi.ashx?ddh=633291215059316&num=1&port=&yys=1&am=0&guolv=y&mt=0&fm=text&spt=%"));
 
-        proxy.setType(QNetworkProxy::HttpProxy);
-        proxy.setHostName(ip);
-        proxy.setPort(port.toInt());
+//        request.setRawHeader("user-agent", "stamaimer");
 
-        qDebug() << ips.size() << index << "current" << proxy.hostName() << proxy.port();
+//        reply = manager.get(request);
 
-        QNetworkProxy::setApplicationProxy(proxy);
+//        synchronous.exec();
 
-    }
-};
+//        QByteArray reply_body;
+
+//        if(reply->error() == QNetworkReply::NoError)
+//        {
+//            reply_body = reply->readAll();
+
+//            qDebug() << reply_body;
+//        }
+//        else
+//        {
+//            qDebug() << reply->error() << reply->errorString();
+//        }
+
+//        QString ip   ;
+//        QString port ;
+
+//        proxy.setType(QNetworkProxy::HttpProxy);
+//        proxy.setHostName(ip);
+//        proxy.setPort(port.toInt());
+
+//        qDebug() << "CURRENT" << proxy.hostName() << proxy.port();
+
+//        QNetworkProxy::setApplicationProxy(proxy);
+//    }
+//};
+
+//QEventLoop Utils::synchronous = QEventLoop();
+
+//QNetworkRequest       Utils::request = QNetworkRequest();
+//QNetworkReply*        Utils::reply = NULL;
+//QNetworkAccessManager Utils::manager = QNetworkAccessManager();
 
 #endif // UTILS_H
