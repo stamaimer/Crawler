1fb46190837189ab7b1eab29d0b152af332ec558
stamaimer <stamaimer@gmail.com>
2014-05-09 20:53:13 +0800

    add header

--- requester.cpp
+++ requester.cpp
@@ -60,6 +60,8 @@ void Requester::run()
 
             request.setUrl(QUrl(walmart->request_url));
 
+            request.setRawHeader("user-agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0");
+
 //            job_scheduler->utils.toggle();
 
             reply = manager.get(request);
